2a23f17e376de5e1b3218b7ab5155b5b932460ba
RYO NAKAE <ryo.n.1129@gmail.com>
2015-09-14 20:37:37 +0900

    三角形が回りながらふわふわする

--- source/assets/javascripts/canvas.js
+++ source/assets/javascripts/canvas.js
@@ -4,19 +4,22 @@ var THREE = require('threejs');
 require('jquery');
 
 var canvas = function(){
-  console.log('canvas dayo~~');
-
+  // 設定
   var width = 600;
   var height = 400;
   var bgColor = 0xf8f8f8;
   var bgAlpha = 0;
 
+
   // シーン
   var scene = new THREE.Scene();
 
+
   // カメラ
-  var camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000);
-  camera.position.set(0,0,-400); //手前に
+  var camera = new THREE.PerspectiveCamera(80, width/height, 1, 1000);
+  camera.position.set(0,0,-200); //手前に
+  camera.lookAt(scene.position);
+
 
   // レンダラ
   var renderer = new THREE.WebGLRenderer({
@@ -28,44 +31,84 @@ var canvas = function(){
   // レンダラをcanvas要素に追加
   document.getElementById('canvas').appendChild(renderer.domElement);
 
-  // グループ
-  var group = new THREE.Object3D();
+
+  // ライト
+  var light0 = new THREE.AmbientLight(0xffffff);
+  var light1 = new THREE.DirectionalLight(0xffffff);
+  var light2 = new THREE.DirectionalLight(0xffffff);
+  light1.position.set(50,300,-50);
+  light2.position.set(-50,300,50);
+
 
   // シェイプ
-  var geometry = new THREE.Geometry();
-  geometry.vertices[0] = new THREE.Vector3(-50,100,0);
-  geometry.vertices[1] = new THREE.Vector3(-100,-150,0);
-  geometry.vertices[2] = new THREE.Vector3(30,20,0);
-  geometry.faces[0] = new THREE.Face3(0,1,2);
+  var geometry = new THREE.Shape();
+  geometry.moveTo(-50,100);
+  geometry.lineTo(-100,-150);
+  geometry.lineTo(30,20);
+  geometry.lineTo(-50,100); //始点に戻る
+  geometry = new THREE.ShapeGeometry(geometry);
+
 
   // マテリアル・テクスチャ
-  var material =  new THREE.MeshBasicMaterial({ color: 0x76faff });
+  var material =  new THREE.MeshLambertMaterial({
+    color: 0x76faff
+  });
   material.side = THREE.DoubleSide; //裏面も見えるようにする
   var triangle =  new THREE.Mesh(geometry,material);
 
+
+  // グループ
+  var group = new THREE.Object3D();
   // グループにtriangle追加
   group.add(triangle);
-  group.position.set(-50,0,0);
+  group.position.set(0,0,0);
 
-  // シーンにグループを追加
+
+  // シーンに要素を追加
   scene.add(group);
+  scene.add(light0);
+  scene.add(light1);
+  scene.add(light2);
+
 
   // アニメーション関数
-  function update() {
+  // くるくる
+  function rotate() {
     // グループを回転
-    // var rotateX = triangle.rotation.x + 0.002;
-    // var rotateY = triangle.rotation.y + 0.005;
-    // var rotateZ = triangle.rotation.z + 0.01;
-    // triangle.rotation.set( rotateX, rotateY, 0 );
+    var rotateX = group.rotation.x + 0.002;
+    var rotateY = group.rotation.y + 0.0005;
+    var rotateZ = group.rotation.z + 0.0001;
+    group.rotation.set( 0, rotateY, rotateZ );
 
     // レンダリング
-    camera.lookAt(scene.position);
     renderer.render(scene,camera);
 
     // 次のアニメーション呼び出す
-    // requestAnimationFrame(update);
+    requestAnimationFrame(rotate);
   }
-  update();
+
+  // ふわふわ
+  var degree = 0;
+  function position() {
+    // グループをふわふわ
+    degree = degree + 1;
+    var radian = degree * Math.PI / 180;
+    var positionY = Math.cos(radian) * 10;
+    group.position.y = positionY;
+
+    // レンダリング
+    renderer.render(scene,camera);
+
+    // 次のアニメーション呼び出す
+    requestAnimationFrame(position);
+  }
+
+
+
+  // アニメーション発火
+  rotate();
+  position();
+
 
   // $(window).on('load resize', function(){
   //   $('#canvas').css({
