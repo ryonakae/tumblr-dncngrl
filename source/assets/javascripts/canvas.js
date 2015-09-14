e65b66169698524199431021a44a7696ee994aba
RYO NAKAE <ryo.n.1129@gmail.com>
2015-09-14 16:57:28 +0900

    update

--- source/assets/javascripts/canvas.js
+++ source/assets/javascripts/canvas.js
@@ -8,18 +8,23 @@ var canvas = function(){
   var width = 600;
   var height = 400;
 
-  var stage = new PIXI.Container(0x000000);
+  var stage = new PIXI.Container();
   var renderer = new PIXI.autoDetectRenderer(width, height, {
+    backgroundColor: 0xffffff,
     antialias: true
   });
 
   document.getElementById('canvas').appendChild(renderer.view);
 
-  var g = new PIXI.Graphics();
+  var triangle = new PIXI.Graphics();
 
-  g.lineStyle(2,0xffffff).moveTo(10,10).lineTo(100,100);
+  triangle.beginFill(0x00FF00);
+  triangle.moveTo(300,0);
+  triangle.lineTo(600,400);
+  triangle.lineTo(0,400);
+  triangle.endFill();
 
-  stage.addChild(g);
+  stage.addChild(triangle);
 
   renderer.render(stage);
 }();
