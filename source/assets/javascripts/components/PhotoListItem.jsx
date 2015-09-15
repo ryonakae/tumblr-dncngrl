411dda73704590e140ebeac10852ca63a3a5da63
RYO NAKAE <ryo.n.1129@gmail.com>
2015-09-15 19:41:52 +0900

    リストごとに個別にアニメーションできるようになった

--- source/assets/javascripts/components/PhotoListItem.jsx
+++ source/assets/javascripts/components/PhotoListItem.jsx
@@ -8,8 +8,60 @@ require('jquery');
 require('velocity');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return{
+      hover: false
+    };
+  },
+
+  // マウスオーバー
   mouseOver: function(){
-    console.log('hover');
+    console.log('over');
+
+    this.setState({
+      hover: true
+    });
+
+    // Native DOM取得してからjQueryオブジェクトに変換
+    // ref属性で指定した名前でrefsからcomponentを参照できる(つらい)
+    var item = $(React.findDOMNode(this.refs.item));
+    var overlay = $(React.findDOMNode(this.refs.overlay));
+
+    // velocity
+    overlay.velocity(
+      {
+        opacity: 1,
+        top: 0
+      },
+      {
+        duration: 800,
+        easing: 'easeInOutQuart'
+      }
+    );
+  },
+
+  // マウスアウト
+  mouseOut: function(){
+    console.log('out');
+
+    this.setState({
+      hover: false
+    });
+
+    var item = $(React.findDOMNode(this.refs.item));
+    var overlay = $(React.findDOMNode(this.refs.overlay));
+
+    // velocity
+    overlay.velocity(
+      {
+        opacity: 0,
+        top: '100%'
+      },
+      {
+        duration: 800,
+        easing: 'easeInOutQuart'
+      }
+    );
   },
 
   render: function(){
@@ -20,16 +72,16 @@ module.exports = React.createClass({
     }
 
     return (
-      <article className="photoList__item" onMouseOver={this.mouseOver} style={{backgroundImage:`url(${this.props.photos[0].original_size.url})`}}>
-        <Link to={`/post/${this.props.id}/${this.props.slug}`} params={{id:this.props.id, slug:this.props.slug}}>
-          <div className="photoList__itemOverlay">
+      <article className="photoList__item" onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} ref='item' style={{backgroundImage:`url(${this.props.photos[0].original_size.url})`}}>
+        <div className="photoList__itemOverlay" ref='overlay'>
+          <Link to={`/post/${this.props.id}/${this.props.slug}`} params={{id:this.props.id, slug:this.props.slug}}>
             <div className="photoList__itemOverlayInner">
               <h2 className='photoList__itemDate'>{moment(new Date(this.props.date)).format('YYYY.M.D')}</h2>
               <ul className="photoList__itemTag">{tags}</ul>
               <div className="photoList__itemNotes">{this.props.noteCount} NOTES</div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </article>
     );
   }
